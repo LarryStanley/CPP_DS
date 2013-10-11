6b8667d2dc6d72a8077f0d9b1a38f85a956a9baa
LarryStanley <kill.xmurderer@gmail.com>
2013-10-11 11:59:42 +0800

    增加更多加法排版

--- First Quiz Warm Up.cc
+++ First Quiz Warm Up.cc
@@ -11,10 +11,10 @@ int main()
 
   //乘法表開始
   for(int i = 1; i<=9; i++){
-    cout << inputInteger << " X " << i << " = "  << inputInteger*i << " ";
+    cout << inputInteger << " X " << i << " = "  << inputInteger*i << setw(3)
     for(int j = 1; j<=i; j++){
       if(j == 1)
-	      cout << setw(6) <<inputInteger;
+	      cout << setw(3) <<inputInteger;
       else
 	      cout << " + " << inputInteger;
     }
