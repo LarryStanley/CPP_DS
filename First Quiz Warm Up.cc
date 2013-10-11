9c4aac907b4b111abc312e74a4cde8fd39dd5c95
LarryStanley <kill.xmurderer@gmail.com>
2013-10-11 11:27:55 +0800

    新增第一次周考題暖身題

---
+++ First Quiz Warm Up.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int inputInteger;
+
+  cin >> inputInteger;
+
+  for(int i = 1; i<=9; i++){
+    cout << inputInteger << " X " << i << " = "  << inputInteger*i << " ";
+    for(int j = 1; j<=i; j++){
+      if(j == 1)
+	      cout <<inputInteger;
+      else
+	      cout << " + " << inputInteger;
+    }
+    if(i != 1)
+      cout << " = " << inputInteger*i;
+    cout << endl;
+  }
+}
